ed6d02cc019663615caebb3dea7fb9d419bd84cf
Jan-Fleming Nickelsen <j.nickelsen@hmmh.de>
2020-08-21 16:09:46 +0200

    Updates

---
+++ precache-manifest.e86165580ee81573e56471bd68a43a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910f940c9bebb66568c4ecee58f84257",
+    "url": "/MovieLib/index.html"
+  },
+  {
+    "revision": "be4187cfb6feabc5dd47",
+    "url": "/MovieLib/static/css/main.31da0569.chunk.css"
+  },
+  {
+    "revision": "62b3e210187fea8d95ea",
+    "url": "/MovieLib/static/js/2.505a179a.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/MovieLib/static/js/2.505a179a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4187cfb6feabc5dd47",
+    "url": "/MovieLib/static/js/main.c06635ab.chunk.js"
+  },
+  {
+    "revision": "2004b3a03a868011bf19",
+    "url": "/MovieLib/static/js/runtime-main.7b89ae2c.js"
+  }
+]);
